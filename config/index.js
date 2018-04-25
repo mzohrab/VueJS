89244f7cb7ce692b5f7444a15b83219c1a213193
mzohrab <zohrab_melikzade@hotmail.com>
2018-04-25 19:19:08 +0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -38,10 +38,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve('C:/inetpub/Testjs/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, 'C:\inetpub\Testjs'),
+    assetsRoot: path.resolve('C:/inetpub/Testjs'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
